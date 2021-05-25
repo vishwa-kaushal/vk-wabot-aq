c3798d16441627a29c3204a060573a369883acfb
vishwa-kaushal <84795836+vishwa-kaushal@users.noreply.github.com>
2021-05-25 23:54:18 +0530

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
-  // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
-  this.sendContact(m.chat, '94764746599', 'Vishwa Kaushal', m)
+  // F 
+      this.sendContact(m.chat, '94764746599', 'Vishwa Kaushal', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
